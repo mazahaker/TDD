0d0953dc3c7c078cc3b55a8e90ebb60e8ce4ea1c
mazahaker <ser9-9-9@ya.ru>
2015-05-04 22:59:20 +0600

    first test

---
+++ Lab3_test/Lab3_test/Clock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_test
+{
+    public partial class Clock : Form
+    {
+        readonly Timer tik;
+        public int hours { get; set; }
+        public int minutes { get; set; }
+        public int second { get; set; }
+
+        public Clock()
+        {
+            tik = new Timer();
+            InitializeComponent();
+        }
+
+        private void Clock_Load(object sender, EventArgs e)
+        {
+            tik.Interval = 10000;
+            tik.Tick += tmrShow_Tick;
+            tik.Enabled = true;
+        }
+
+        private void tmrShow_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Lab3_test/Lab3_test/test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab3_test
+{
+    [TestClass]
+    public class Test_gamer
+    {
+
+
+        [TestMethod]
+        public void Test_Create_Clock()
+        {
+            Clock watch = new Clock();
+            Assert.IsNotNull(watch);
+        }
+
+        [TestMethod]
+        public void Test_Create_Hours()
+        {
+            Clock watch = new Clock();
+            Assert.IsNotNull(watch.hours);
+        }
+
+
+    }
+}
